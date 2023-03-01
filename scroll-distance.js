38782eb54499010ada48a5d577d940bc1d969825
WallardW <vegard.wik@gmail.com>
2023-03-01 16:22:42 +0100

    timeline wip

--- scroll-distance.js
+++ scroll-distance.js
@@ -1,5 +1,26 @@
-// Get the element with id="scroll-marker"
-let scrollTracker = document.getElementById("scroll-marker");
+//get the element with id="scroll-marker"
+const scrollTracker = document.getElementById("scroll-marker");
 
-// Update its top offset value based on the current scroll position
-scrollTracker.style.top = window.scrollY + "px";
+//get the "main" element
+const main = document.querySelector('main');
+
+//get the timeline element
+const timeline = document.getElementsByClassName("timeline")
+
+//get the height of the "main" element
+const mainHeight = main.offsetHeight;
+
+//get distance from "main" to top of root element
+const offsetTop = main.offsetTop;
+
+//get height of the timeline element
+const timeHeight = timeline.offsetHeight;
+
+// Update scroll-marker top offset value based on the current scroll position
+window.addEventListener('scroll', function () {
+    if (window.scrollY - offsetTop < 0) {
+    } else if (window.scrollY - offsetTop > 0) {
+        scrollTracker.style.top = (window.scrollY - offsetTop) * (timeHeight / mainHeight) + "px";
+
+    }
+})
